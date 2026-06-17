f3b31803cc01e14a443a5b548438d2fa3509148d
Naveen <nk986960@gmail.com>
2026-06-18 00:01:22 +0530

    added to take API_BASE_URL from env

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import { store } from "../app/store";
 import { setCredentials, clearCredentials} from '../features/authSlice'
 
 export const axiosInstance = axios.create({
-	baseURL: "http://localhost:3000/api",
+	baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
     withCredentials: true
 });
 
